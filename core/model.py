3493c272485be64185ca8c712b65fab2dbe8974a
koukyo1994 <arabiannight1994@yahoo.co.jp>
2019-02-01 18:18:02 +0900

    Changed Logistic parameters to tuned one

--- core/model.py
+++ core/model.py
@@ -8,9 +8,12 @@ def lr_model():
     steps = [("vec", Count(max_features=20000, ngram_range=(1, 3))),
              ("tfidf", Tfidf()),
              ("clf",
-              LR(solver="lbfgs",
+              LR(penalty="l2",
+                 solver="saga",
                  multi_class="auto",
                  random_state=42,
                  max_iter=1e4,
-                 n_jobs=-1))]
+                 n_jobs=-1,
+                 C=9.930234540337622,
+                 tol=0.0007738614855921237))]
     return Pipeline(steps=steps)
